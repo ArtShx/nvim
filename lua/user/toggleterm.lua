c601b0fe3d20c2893291c248103eedfd2b524539
Arthur Miada <artshx@gmail.com>
2025-02-27 23:33:11 -0300

    merge: not using powershell on unix

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -16,8 +16,7 @@ toggleterm.setup({
   persist_size = true,
   direction = "horizontal",
   close_on_exit = true,
-  -- shell = vim.o.shell,
-  shell = "powershell.exe",
+  shell = vim.o.shell,
   float_opts = {
     border = "curved",
     winblend = 0,
